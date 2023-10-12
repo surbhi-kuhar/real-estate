c7b63ddb9dad726c93804ac0cb8eb9ac193375be
surbhi-kuhar <surbhikuhar07@gmail.com>
2023-10-12 16:17:24 +0530

    creation of user model

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
-mongoose.connect();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected')
+}).catch((err)=>{
+    console.log(err);
+});
+
 
 
 app.listen(3000,()=>{
