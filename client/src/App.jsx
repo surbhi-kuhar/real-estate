9fee251318380c0b090acbf2d16743e14de64a7f
surbhi-kuhar <surbhikuhar07@gmail.com>
2023-11-10 22:53:41 +0530

    fix app.jsx errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Signin from "./pages/signin";
+import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
-import About from "./pages/about";
-import Profile from "./pages/profile";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import Listing from "./pages/Listing";
